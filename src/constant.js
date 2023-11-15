a7c60f0e4c52dec54fd245ed6f023bc402dfe6ca
Jeeshan Khan <jeeshankhan@Jeeshans-MacBook-Air.local>
2023-11-15 13:19:24 +0530

    Adding models and more functionality and utils

---
+++ src/constant.js
@@ -0,0 +1,1 @@
+export const connectDb = "Professniol_Db"
---
+++ src/database/connectDb.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import { connectDb } from "../constant.js"
+
+// Function to connect to the database
+const connectToDatabase = async () => {
+    try {
+        const connectionIns = await mongoose.connect(`${process.env.MONGODB_URI}/${connectDb}`)
+        console.log(`\n MongoDB Connected On Host! ${connectionIns.connection.host}`);
+    } catch (error) {
+        // Log any errors
+        console.error("Error connecting to database: ", error);
+        process.exit(1)
+    }
+};
+
+
+// Export the connectToDatabase function
+export default connectToDatabase;
